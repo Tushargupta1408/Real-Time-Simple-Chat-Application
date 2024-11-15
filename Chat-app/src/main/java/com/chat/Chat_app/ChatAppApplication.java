3e876fd6729452d8fe13907e836c84d3d3413750
Tushar Gupta <113227091+Tushargupta1408@users.noreply.github.com>
2024-11-15 21:20:25 +0530

    Add files via upload

---
+++ Chat-app/src/main/java/com/chat/Chat_app/ChatAppApplication.java
@@ -0,0 +1,18 @@
+package com.chat.Chat_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class ChatAppApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ChatAppApplication.class, args);
+	}
+
+}
+
